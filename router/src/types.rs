c6562353e5ff1edc87aba2b43b4b51181e2a9e15
Thomas Gideon <cmdln@thecommandline.net>
2018-03-02 17:43:04 -0500

    Improve and add tests.

--- router/src/types.rs
+++ router/src/types.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use error::*;
 
 const PATH_LIMIT: usize = 25;
@@ -11,113 +13,99 @@ impl Route {
     /// Create a new route mapping with an index component and no handler.
     pub fn new() -> Self {
         Route {
-            root: PathComp::Path {
-                path: "".to_owned(),
-                next: vec![],
-                handler: None,
-            },
+            root: PathComp::new("", None),
         }
     }
 
     /// Add the specified handler at the given route.
     pub fn add(&mut self, route: &str, handler: String) -> Result<()> {
         // TODO re-factor into iterative
-        fn go(idx: usize, tokens: &[&str], current: &mut PathComp, handler: String) {
-            let replace_idx = Route::pos_comp(tokens[idx], current);
-            let to_update = if idx + 1 == tokens.len() {
-                PathComp::path(tokens[idx], Some(handler))
-            } else {
-                let mut to_update = PathComp::path(tokens[idx], None);
-                go(idx + 1, tokens, &mut to_update, handler);
-                to_update
-            };
-            if let Some(replace_idx) = replace_idx {
-                let PathComp::Path { ref mut next, .. } = *current;
-                next[replace_idx] = to_update;
-            } else {
-                let PathComp::Path { ref mut next, .. } = *current;
-                next.push(to_update);
+        fn go(
+            idx: usize,
+            tokens: &[&str],
+            current: &mut HashMap<String, PathComp>,
+            handler: String,
+        ) {
+            if let Some(comp) = current.get_mut(tokens[idx]) {
+                if idx + 1 == tokens.len() {
+                    comp.handler = Some(handler);
+                    return;
+                } else {
+                    go(idx + 1, tokens, &mut comp.next, handler);
+                    return;
+                }
+            }
+
+            if current.get(tokens[idx]).is_none() {
+                let mut comp = PathComp::new(tokens[idx], None);
+                if idx + 1 == tokens.len() {
+                    comp.handler = Some(handler);
+                    current.insert(tokens[idx].to_owned(), comp);
+                } else {
+                    go(idx + 1, tokens, &mut comp.next, handler);
+                    current.insert(tokens[idx].to_owned(), comp);
+                }
             }
         }
 
-        // replace the root of the radix trie
-        if "/" == route {
-            self.root = PathComp::path("", Some(handler));
-            return Ok(());
+        let tokens = path_to_tokens(route)?;
+        if tokens.len() == 1 {
+            self.root.handler = Some(handler)
+        } else {
+            go(1, &tokens, &mut self.root.next, handler);
         }
-        if !route.starts_with("/") {
-            bail!("Route to add must start with a slash (/)")
-        }
-        let tokens: Vec<&str> = route.split("/").collect();
-        // a short term protection until re-factoring the recursive code to be iterative
-        if tokens.len() > PATH_LIMIT {
-            bail!("Currently cannot add a path with more than 25 components")
-        }
-        // since the root node replacement is handled above, start with the next level and path
-        // component
-        go(1, &tokens, &mut self.root, handler);
+
         Ok(())
     }
 
-    pub fn dispatch(&self, request_path: &str) -> Result<Option<String>> {
+    pub fn dispatch<'a>(&'a self, request_path: &str) -> Result<&'a Option<String>> {
         // TODO re-factor into iterative
-        fn go(idx: usize, tokens: &[&str], current: &PathComp) -> Result<Option<String>> {
-            let PathComp::Path {
-                ref path,
-                ref handler,
-                ..
-            } = *current;
-            if path == tokens[idx] {
+        fn go<'a>(
+            idx: usize,
+            tokens: &[&str],
+            current: &'a HashMap<String, PathComp>,
+        ) -> Result<&'a Option<String>> {
+            if let Some(current) = current.get(tokens[idx]) {
                 if idx + 1 == tokens.len() {
-                    Ok(handler.clone())
-                } else if let Some(current) = Route::find_comp(tokens[idx + 1], current) {
-                    go(idx + 1, tokens, current)
+                    Ok(&current.handler)
                 } else {
-                    bail!("Handler not found at path!")
+                    go(idx + 1, tokens, &current.next)
                 }
             } else {
                 bail!("Path not found!")
             }
         }
-        let tokens: Vec<&str> = request_path.split("/").collect();
-        // a short term protection until re-factoring the recursive code to be iterative
-        if tokens.len() > PATH_LIMIT {
-            bail!("Currently cannot search for a path with more than 25 components")
-        }
-        go(0, &tokens, &self.root)
-    }
 
-    fn pos_comp(to_find: &str, to_search: &mut PathComp) -> Option<usize> {
-        let PathComp::Path { ref next, .. } = *to_search;
-        next.iter().position(|comp| {
-            let PathComp::Path { ref path, .. } = *comp;
-            path == to_find
-        })
+        let tokens = path_to_tokens(request_path)?;
+        go(1, &tokens, &self.root.next)
     }
+}
 
-    fn find_comp<'a>(to_find: &str, to_search: &'a PathComp) -> Option<&'a PathComp> {
-        let PathComp::Path { ref next, .. } = *to_search;
-        next.iter().find(|comp| {
-            let &PathComp::Path { ref path, .. } = *comp;
-            path == to_find
-        })
+fn path_to_tokens(path: &str) -> Result<Vec<&str>> {
+    let path = path.trim_right_matches('/');
+    let tokens: Vec<&str> = path.split("/").collect();
+    // a short term protection until re-factoring the recursive code to be iterative
+    if tokens.len() > PATH_LIMIT {
+        bail!("Currently cannot work with a path with more than 25 components")
     }
+    if tokens[0] != "" {
+        bail!("Paths must start with a slash (/)")
+    }
+    Ok(tokens)
 }
 
 #[derive(Debug, PartialEq)]
-pub enum PathComp {
-    Path {
-        path: String,
-        next: Vec<PathComp>,
-        handler: Option<String>,
-    },
+pub struct PathComp {
+    path: String,
+    next: HashMap<String, PathComp>,
+    handler: Option<String>,
 }
 
 impl PathComp {
-    fn path(path: &str, handler: Option<String>) -> PathComp {
-        PathComp::Path {
+    fn new(path: &str, handler: Option<String>) -> PathComp {
+        PathComp {
             path: path.to_owned(),
-            next: vec![],
+            next: HashMap::new(),
             handler,
         }
     }
@@ -129,30 +117,50 @@ mod test {
 
     #[test]
     pub fn test_add() {
+        let mut expected = PathComp::new("", None);
+        let mut foo = PathComp::new("foo", None);
+        let bar = PathComp::new("bar", Some(String::from("Bar")));
+        foo.next.insert(String::from("bar"), bar);
+        expected.next.insert(String::from("foo"), foo);
         let mut route = Route::new();
         route
             .add("/foo/bar", String::from("Bar"))
             .expect("Should have added route without error");
-        assert_eq!(
-            PathComp::Path {
-                path: "".to_owned(),
-                handler: None,
-                next: vec![
-                    PathComp::Path {
-                        path: "foo".to_owned(),
-                        handler: None,
-                        next: vec![
-                            PathComp::Path {
-                                path: "bar".to_owned(),
-                                handler: Some(String::from("Bar")),
-                                next: vec![],
-                            },
-                        ],
-                    },
-                ],
-            },
-            route.root
+        assert_eq!(expected, route.root);
+    }
+
+    #[test]
+    pub fn test_add_two() {
+        let foo = sub_route2("foo", "bar", "baz");
+        let mut expected = PathComp::new("", None);
+        expected.next.insert(String::from("foo"), foo);
+        let mut route = Route::new();
+        route
+            .add("/foo/bar", String::from("BAR"))
+            .expect("Should have added route without error");
+        route
+            .add("/foo/baz", String::from("BAZ"))
+            .expect("Should have added route without error");
+        assert_eq!(expected, route.root);
+    }
+
+    #[test]
+    pub fn test_add_extend() {
+        let mut expected = PathComp::new("", None);
+        let mut foo = PathComp::new("foo", Some(String::from("Foo")));
+        foo.next.insert(
+            String::from("bar"),
+            PathComp::new("bar", Some(String::from("Bar"))),
         );
+        expected.next.insert(String::from("foo"), foo);
+        let mut route = Route::new();
+        route
+            .add("/foo/bar/", String::from("Bar"))
+            .expect("Should have added route without error");
+        route
+            .add("/foo/", String::from("Foo"))
+            .expect("Should have added route without error");
+        assert_eq!(expected, route.root);
     }
 
     #[test]
@@ -165,12 +173,43 @@ mod test {
         if let Ok(found) = found {
             assert_eq!(
                 Some(String::from("Bar")),
-                found,
+                *found,
                 "Could not find handler, {:?}",
                 route.root
             );
         } else {
             panic!("Error searching {:?}", found.unwrap_err());
         }
+    }
+
+    #[test]
+    pub fn test_partial() {
+        let mut route = Route::new();
+        route
+            .add("/foo/bar", String::from("Bar"))
+            .expect("Should have added route without error");
+        let found = route.dispatch("/foo");
+        if let Ok(found) = found {
+            assert_eq!(
+                None, *found,
+                "Should not have found handler, {:?}",
+                route.root
+            );
+        } else {
+            panic!("Error searching {:?}", found.unwrap_err());
+        }
+    }
+
+    fn sub_route2(parent: &str, first: &str, second: &str) -> PathComp {
+        let mut comp = PathComp::new(parent, None);
+        comp.next.insert(
+            first.to_owned(),
+            PathComp::new(first, Some(String::from(first.to_uppercase()))),
+        );
+        comp.next.insert(
+            second.to_owned(),
+            PathComp::new(second, Some(String::from(second.to_uppercase()))),
+        );
+        comp
     }
 }
