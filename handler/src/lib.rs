43ce2744bd8ec8732892401d7f42d8bd168d5c24
Thomas Gideon <cmdln@thecommandline.net>
2018-03-24 14:55:02 -0400

    Add convenience for wrapping fn with Handler, then Service

--- handler/src/lib.rs
+++ handler/src/lib.rs
@@ -58,6 +58,7 @@ where
     type Error = hyper::Error;
     type Future = ServiceFuture;
 
+    /// Dispatches to the owned `Handler`, marshalling success or error into the response.
     fn call(&self, request: Request) -> Self::Future {
         let http_request = HttpRequest::Raw(request);
         match self.handler.handle(http_request) {
@@ -68,6 +69,34 @@ where
                     .with_body(error.body()),
             )),
         }
+    }
+}
+
+/// Accepts a function or closure that takes an `HttpRequest` and returns a compatible `Result`.
+pub fn handler_fn<F, E>(func: F) -> HandlerService<HandlerFn<F, E>, E>
+where
+    F: Fn(HttpRequest) -> Result<Response, E>,
+    E: IntoResponse,
+{
+    HandlerService::new(HandlerFn { func })
+}
+
+/// Holds a function to dispatch to via its impl of `Handler<E>`.
+pub struct HandlerFn<F, E>
+where
+    F: Fn(HttpRequest) -> Result<Response, E>,
+    E: IntoResponse,
+{
+    func: F,
+}
+
+impl<F, E> Handler<E> for HandlerFn<F, E>
+where
+    F: Fn(HttpRequest) -> Result<Response, E>,
+    E: IntoResponse,
+{
+    fn handle(&self, req: HttpRequest) -> Result<Response, E> {
+        (self.func)(req)
     }
 }
 
@@ -86,8 +115,10 @@ where
 
 #[cfg(test)]
 mod tests {
+    extern crate luminal_router;
     extern crate tokio_core;
 
+    use self::luminal_router::Router;
     use futures::Stream;
     use hyper::Method;
 
@@ -149,6 +180,58 @@ mod tests {
             Method::Get,
             "/foo",
             (&StatusCode::InternalServerError, "Error"),
+        );
+    }
+
+    fn test_fn(_req: HttpRequest) -> Result<Response, TestError> {
+        Ok(Response::new()
+            .with_status(StatusCode::Ok)
+            .with_body(String::from("test")))
+    }
+
+    #[test]
+    fn test_fn_route() {
+        let handler = TestHandler::Success(String::from("Success"));
+        let service = HandlerService::new(handler);
+        let router = Router::new();
+
+        let router = router
+            .get("/foo", service)
+            .expect("Should have been able to add handler")
+            .get("/bar", handler_fn(test_fn))
+            .expect("Should have been able to add handler fn");
+
+        assert_route(&router, Method::Get, "/foo", (&StatusCode::Ok, "Success"));
+        assert_route(&router, Method::Get, "/bar", (&StatusCode::Ok, "test"));
+    }
+
+    fn assert_route(service: &Router, method: Method, uri: &str, expected: (&StatusCode, &str)) {
+        let uri = uri.parse()
+            .expect("Should have been able to convert to uri");
+        let req: Request<Body> = Request::new(method, uri);
+
+        let work = service.call(req);
+
+        let mut core = Core::new().expect("Should have been able to create core");
+
+        let response = core.run(work)
+            .expect("Should have been able to run router call");
+
+        assert_eq!(
+            *expected.0,
+            response.status(),
+            "Should have received {} status.",
+            expected.0
+        );
+
+        let body = core.run(response.body().concat2())
+            .expect("Should have been able to resolve body concat");
+        let body: &[u8] = &body.to_vec();
+
+        assert_eq!(
+            expected.1.as_bytes(),
+            body,
+            "Should have received correct body content"
         );
     }
 
